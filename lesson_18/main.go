809c513fa80b4ac9f51bd5abd669024301474f0e
Oksana Onilova <onilovaoksana@gmail.com>
2024-07-21 09:39:47 +0100

    Add hometask 18

---
+++ lesson_18/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"docker_postgres/database"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	database.ConnectDb()
+	app := fiber.New()
+
+	setupRoutes(app)
+
+	app.Listen(":3000")
+}
---
+++ lesson_18/routes.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"docker_postgres/handlers"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/", handlers.ListFacts)
+
+	app.Post("/fact", handlers.CreateFact)
+}
